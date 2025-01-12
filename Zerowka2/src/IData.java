348c4f01f3423c46ad7a1842c0d233486b72c7f3
maroo <122287678+maroo602@users.noreply.github.com>
2025-01-12 16:56:37 +0100

    Delete Zerowka2 directory

--- Zerowka2/src/IData.java
+++
@@ -1,6 +0,0 @@
-import java.time.LocalDateTime;
-
-public interface IData {
-    void ustawDate(LocalDateTime data);
-    boolean sprawdzDate(LocalDateTime data);
-}
--- Zerowka2/src/IZarzadzaj.java
+++
@@ -1,9 +0,0 @@
-public interface IZarzadzaj {
-    void dodajAutobus(int iloscMiejsc);
-
-    void dodajPociag(int iloscMiejsc, int dlugoscTrasy);
-
-    void usunOstatni();
-
-    void wyczysc();
-}
